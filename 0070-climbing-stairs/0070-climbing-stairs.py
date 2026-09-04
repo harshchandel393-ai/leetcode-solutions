b2c7892c77f6a886fc3e49bfcbf1177fe890f82b
Harsh <harshchandel393@gmail.com>
2026-09-04 22:56:57 +0530

    Time: 0 ms (100%), Space: 19.2 MB (53.55%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,15 +1,13 @@
 class Solution:
-    def climbStairs(self, n):
-        memo = {}
-
+    def climbStairs(self, n: int) -> int:
+        cache = [-1] * n
         def dfs(i):
             if i >= n:
                 return i == n
-
-            if i in memo:
-                return memo[i]
-
-            memo[i] = dfs(i + 1) + dfs(i + 2)
-            return memo[i]
+            if cache[i] != -1:
+                return cache[i]
+            cache[i] = dfs(i + 1) + dfs(i + 2)
+            return cache[i]
 
         return dfs(0)
+        
